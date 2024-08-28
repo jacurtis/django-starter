d75dceb2d5afff373089958462f7166eb45b780e
Alex Curtis <jalexandercurtis@gmail.com>
2024-08-27 22:25:40 -0600

    feat: Add password reset form

--- config/settings.py
+++ config/settings.py
@@ -131,3 +131,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
